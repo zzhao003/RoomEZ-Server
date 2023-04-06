570536653b59529c9e2b400dd9024f7ecd2661c5
zzhao003 <zzhao003@gmail.com>
2023-04-05 23:17:31 -0400

    adds get user route

--- route/feed.js
+++ route/feed.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const knex = require("knex")(require("../knexfile"));
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  knex("user")
+    .select(
+      "id",
+      "first_name",
+      "age",
+      "gender",
+      "profession",
+      "budget",
+      "area",
+      "pet",
+      "movein_date",
+      "about"
+    )
+    .then((data) => {
+      const randomNum = Math.floor(Math.random() * data.length);
+      res.status(200).json(data[randomNum]);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving user ${err}`));
+});
 
 router.get("/:id", (req, res) => {});
 
